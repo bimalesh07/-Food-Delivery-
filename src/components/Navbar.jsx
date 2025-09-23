6ecd7da6f8b082622a1fac0c3b0c3e09f9676651
Bimalesh Kumar Yadav <bimaleshk07@gmail.com>
2025-09-23 17:29:49 +0545

    Refactor Navbar component and CSS styles for improved layout and readability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,28 @@
 import React from 'react'
 import { assets } from '../assets/assets';
 import "./Navbar.css";
-import { useEffect } from 'react';
-const Navbar = ({Theme,setTheme}) => {
 
-useEffect(() => {
-    if(Theme==='dark'){
-      document.body.classList.add('dark')
-    }else{
-        document.body.classList.remove('dark')
-    }
-    localStorage.setItem('theme',Theme)
-
-}, [Theme])
-
+const Navbar = () => {
 
   return (
-
-    <div className='navbar'>
-        <img src={assets.logo}></img>
-        <ul className='ul'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Contact</a></li>
-            <li><a href='#'>Servies</a></li>
-            <li><a href='#'>Home</a></li>
-        </ul>
-        <button onClick={()=>setTheme('dark')}>SETDARK</button>
-        <button onClick={()=>setTheme('light')}>SETLIGHT</button>
+    <div className="navbar">
+      <img src={assets.logo}></img>
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>menu</li>
+        <li>Mobile-app</li>
+        <li>Contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon}></img>
+      </div>
+      <div className="navbar-search-icon">
+        <img src={assets.basket_icon}></img>
+        <div className='dot'></div>
+      </div>
+      <button className='btn'>Sign in</button>
     </div>
-  )
+  );
 }
 
 export default Navbar
