e6e6be2d8e7aec156d2be1598633c9dc44b30be3
Bimalesh Kumar Yadav <bimaleshk07@gmail.com>
2025-09-23 20:47:18 +0545

    Add routing and new pages for Home, Cart, and Placeorder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import { useState } from 'react'
+import {  Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Placeorder from './pages/Placeorder/Placeorder';
+import Cart from './pages/Cart/Cart';
 function App() {
-const [Theme, setTheme] = useState(()=>localStorage.getItem('theme')||'light')
-
   return (
     <>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+      <Route path='/order' element={<Placeorder/>}></Route>
+    </Routes>
 
     </>
   )
