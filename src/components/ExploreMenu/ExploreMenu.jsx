b902b8499ec846ecf1ffc61d4ded9b3e742a422c
Bimalesh Kumar Yadav <bimaleshk07@gmail.com>
2025-09-24 20:19:53 +0545

    Update ExploreMenu CSS and component layout

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from "../../assets/assets";
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory }) => {
 
   return (
-    <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our menu</h1>
-        <p className='explore-menu-text'Choose from a diverse menu featuring a delectable array of></p>
-        <div className='explore-menu-list'>
-            {menu_list.map((menu,index)=>(
-                <div className='explore-menu-items'></div>
-            ))}
-
-        </div>
-
-
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring delectable array of dishes our
+        mission is to satisfy your craving and elevate your dining exprerience
+        meal at a time
+      </p>
+      <div className="explore-menu-list-item">
+        {menu_list.map((item, index) => (
+          <div onClick={()=>setCategory(prev=>prev===item.menu_name? "All":item.menu_name)} key={index} className="explore-menu-items">
+            <img
+              className={category=== item.menu_name? "active" : ""}
+              src={item.menu_image}
+            ></img>
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
     </div>
-  )
+  );
 }
 
 export default ExploreMenu
